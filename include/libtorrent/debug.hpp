03c0e888305c4b6b6cc67e355cbc17a41e9d74bc
Nikolas Garofil <nikolas@garofil.be>
2012-08-22 11:24:51 +0200

    complete() isn't a member

--- include/libtorrent/debug.hpp
+++ include/libtorrent/debug.hpp
@@ -70,7 +70,7 @@ namespace libtorrent
 			try
 			{
 #endif
-				fs::path dir(fs::complete(logpath / ("libtorrent_logs" + boost::lexical_cast<std::string>(instance))));
+				fs::path dir(fs::system_complete(logpath / ("libtorrent_logs" + boost::lexical_cast<std::string>(instance))));
 				if (!fs::exists(dir)) fs::create_directories(dir);
 				m_file.open((dir / filename).string().c_str(), std::ios_base::out | (append ? std::ios_base::app : std::ios_base::out));
 				*this << "\n\n\n*** starting log ***\n";
